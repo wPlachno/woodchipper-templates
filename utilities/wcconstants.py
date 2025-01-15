9f295dfe6b0b837a3bc6afe0bcc29961e599a205
wPlachno <wjplachno@gmail.com>
2025-01-15 14:22:08 -0800

    01/15/25 - Formalized COLOUR and BG as the main 16 colors, applied to the text or the text background, respectively, and expanded clr() to accept multiple codes at a time. (wplachno)

--- utilities/wcconstants.py
+++ utilities/wcconstants.py
@@ -1,71 +1,121 @@
 """
 wcconstants.py
 Created by Will Plachno on 09/07/24
-Version 0.0.0.10
+Version: 0.0.1.012
+Last Changes: 01/15/2025
 
 Woodchipper Constants
 An assortment of helpful strings and other constants.
 """
 
-EMPTY=""
-NL="\n"     # New Line
-FS="/"      # Forward Slash
-BS="\\"     # Back Slash
-QU="\""     # Quotes
-CD=", "     # Comma Delimiter
-SDL=":"     # IS THIS USED?
-ES="."      # End Statement
-EL=".\n"    # End line
+class KEY:
+    EMPTY=""
+    NL="\n"     # New Line
+    FS="/"      # Forward Slash
+    BS="\\"     # Back Slash
+    QU="\""     # Quotes
+    CD=", "     # Comma Delimiter
+    SDL=":"     # IS THIS USED?
+    ES="."      # End Statement
+    EL=".\n"    # End line
 
-CS=": "     # Colon Separator
-DH=" - "    # Description Hyphen
+    CS=": "     # Colon Separator
+    DH=" - "    # Description Hyphen
 
 OOB=-1
 
-OP0="{0}"
-OP1="{1}"
-OP2="{2}"
-OP3="{3}"
-OP4="{4}"
-OP5="{5}"
+OP = {
+    0:"{0}",
+    1:"{1}",
+    2:"{2}",
+    3:"{3}",
+    4:"{4}",
+    5:"{5}"
+}
 
-COLOR_RED='\033[0;31m'
-COLOR_GREY='\033[90m'
-COLOR_YELLOW='\033[93m'
-COLOR_BLUE='\033[94m'
-COLOR_DARK_YELLOW='\033[0;33m'
-COLOR_GREEN='\033[0;32m'
-COLOR_DARK_GREEN='\033[2;32m'
-COLOR_PURPLE='\033[0;35m'
-COLOR_BLACK='\033[0;30m'
-COLOR_WHITE='\033[37m'
-COLOR_DEFAULT='\033[0m'
+class COLOUR:
 
-COLOR_SUPER=COLOR_PURPLE
-COLOR_SUB=COLOR_GREY
-COLOR_SUBSIB=COLOR_DARK_GREEN
-COLOR_SIBLING=COLOR_DARK_YELLOW
-COLOR_OPTION=COLOR_BLUE
-COLOR_QUOTE=COLOR_GREEN
-COLOR_ACTIVE=COLOR_GREEN
-COLOR_CANCEL=COLOR_RED
+    DEFAULT='\033[0m'
 
-CL_DESC_ACTIVE = COLOR_ACTIVE+"Active"+COLOR_DEFAULT
-CL_DESC_INACTIVE = COLOR_CANCEL+"Inactive"+COLOR_DEFAULT
-CL_DESC_SUCCESS = COLOR_ACTIVE+"Success"+COLOR_DEFAULT
-CL_DESC_FAILURE = COLOR_CANCEL+"Failure"+COLOR_DEFAULT
+    BLACK       ='\033[38;5;0m'
+    DARK_RED    ='\033[38;5;1m'
+    DARK_GREEN  ='\033[38;5;2m'
+    DARK_YELLOW ='\033[38;5;3m'
+    DARK_BLUE   ='\033[38;5;4m'
+    DARK_PURPLE ='\033[38;5;5m'
+    DARK_TEAL   ='\033[38;5;6m'
+    DARK_WHITE  ='\033[38;5;7m'
 
-CL_DESC_ATTRIBUTE = COLOR_SIBLING+OP0+COLOR_DEFAULT+CS+OP1+NL
-CL_DESC_TASK = COLOR_SUPER+"Task"+COLOR_DEFAULT+CS
-CL_DESC_UNIMPLEMENTED = " ["+COLOR_CANCEL+"UNIMPLEMENTED"+COLOR_DEFAULT+"]"
-CL_DESC_MODE_CONFIG = CL_DESC_TASK+COLOR_SUB+"Config"+COLOR_DEFAULT+DH+COLOR_SIBLING+OP0+COLOR_DEFAULT+CS+OP1+NL
-CL_DESC_CONFIG_ERROR = CL_DESC_TASK+COLOR_SUB+"Config"+COLOR_DEFAULT+DH+COLOR_CANCEL+OP0+COLOR_DEFAULT+NL
-CL_DESC_TEST_PASS = CL_DESC_TASK+COLOR_SUB+"Test Passed"+COLOR_DEFAULT+DH+COLOR_ACTIVE+OP0+COLOR_DEFAULT+NL
-CL_DESC_TEST_FAIL = CL_DESC_TASK+COLOR_SUB+"Test Failed"+COLOR_DEFAULT+DH+COLOR_CANCEL+OP0+COLOR_DEFAULT+NL
+    GREY        ='\033[38;5;8m'
+    RED         ='\033[38;5;9m'
+    GREEN       ='\033[38;5;10m'
+    YELLOW      ='\033[38;5;11m'
+    BLUE        ='\033[38;5;12m'
+    PURPLE      ='\033[38;5;13m'
+    TEAL        ='\033[38;5;14m'
+    WHITE       ='\033[38;5;15m'
 
-READ = "r"
-WRITE = "w"
-EXCLUSIVE_CREATION = "x"
+class BG:
+
+    DEFAULT='\033[0m'
+
+    BLACK       ='\033[48;5;0m'
+    DARK_RED    ='\033[48;5;1m'
+    DARK_GREEN  ='\033[48;5;2m'
+    DARK_YELLOW ='\033[48;5;3m'
+    DARK_BLUE   ='\033[48;5;4m'
+    DARK_PURPLE ='\033[48;5;5m'
+    DARK_TEAL   ='\033[48;5;6m'
+    DARK_WHITE  ='\033[48;5;7m'
+
+    GREY        ='\033[48;5;8m'
+    RED         ='\033[48;5;9m'
+    GREEN       ='\033[48;5;10m'
+    YELLOW      ='\033[48;5;11m'
+    BLUE        ='\033[48;5;12m'
+    PURPLE      ='\033[48;5;13m'
+    TEAL        ='\033[48;5;14m'
+    WHITE       ='\033[48;5;15m'
+
+class COLOR:
+    SUPER=  COLOUR.PURPLE
+    SUB=    COLOUR.GREY
+    SUBSIB= COLOUR.DARK_GREEN
+    SIBLING=COLOUR.DARK_YELLOW
+    OPTION= COLOUR.DARK_BLUE
+    QUOTE=  COLOUR.DARK_TEAL
+    ACTIVE= COLOUR.GREEN
+    CANCEL= COLOUR.DARK_RED
+    DEFAULT=COLOUR.DEFAULT
+
+def clr(text, *color):
+    codes=color[0]
+    for code in color[1:]:
+        codes += code
+    return codes + text + COLOUR.DEFAULT
+
+class CL_GENERAL:
+    ACTIVE = clr("Active", COLOR.ACTIVE)
+    INACTIVE = clr("Inactive", COLOR.CANCEL)
+    SUCCESS = clr("Success", COLOR.ACTIVE)
+    FAILURE = clr("Failure", COLOR.CANCEL)
+    ATTRIBUTE = clr(OP[0], COLOR.SIBLING)+KEY.CS+OP[1]+KEY.NL
+    TASK = clr("Task", COLOR.SUPER)+KEY.CS
+    UNIMPLEMENTED = " ["+clr("UNIMPLEMENTED", COLOR.CANCEL)+"]"
+
+class CL_TASK:
+    MODE_CONFIG =   CL_GENERAL.TASK+clr("Config", COLOR.SUB)        +KEY.DH+clr(OP[0], COLOR.SIBLING)+KEY.CS+OP[1]+KEY.NL
+    CONFIG_ERROR =  CL_GENERAL.TASK+clr("Config", COLOR.SUB)        +KEY.DH+clr(OP[0], COLOR.CANCEL) +KEY.NL
+    TEST_PASS =     CL_GENERAL.TASK+clr("Test Passed", COLOR.SUB)   +KEY.DH+clr(OP[0], COLOR.ACTIVE) +KEY.NL
+    TEST_FAIL =     CL_GENERAL.TASK+clr("Test Failed", COLOR.SUB)   +KEY.DH+clr(OP[0], COLOR.CANCEL) +KEY.NL
+
+class FILE_IO:
+    READ = "r"
+    WRITE = "w"
+    EXCLUSIVE_CREATION = "x"
+
+def format_success(success):
+    return CL_GENERAL.SUCCESS if success else CL_GENERAL.FAILURE
 
 DEBUG = "debug"
 VERBOSE = "verbose"
@@ -88,3 +138,13 @@ class Verbosity:
     NORMAL = 2 # Includes warnings and big errors
     DEBUG = 3 # Print all messages possible
 
+class MODE:
+    NONE = "none"
+    TEST = "test"
+class PRINT:
+    ERROR = clr("ERROR: ", COLOR.CANCEL)+OP[0]+KEY.NL
+    KVP = clr(OP[0], COLOR.SIBLING)+KEY.CS+OP[1]
+    LABEL = clr(OP[0], COLOR.SUPER)+KEY.CS
+class RESULTS:
+    SUCCESS = "success"
+    ERROR = "error"
